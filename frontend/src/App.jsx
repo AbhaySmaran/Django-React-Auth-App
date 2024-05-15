2af63f1dde17078e84086004d55939e83e82ff70
AbhaySmaran <dasabhaysmaran@gmail.com>
2024-05-15 23:42:34 +0530

    updated commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import LoginReg from './components/pages/LoginReg';
 import SendPasswordResetEmail from './components/pages/auth/SendPasswordResetEmail';
+import ResetPassword from './components/pages/auth/ResetPassword';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +20,9 @@ function App() {
             <Route path='contact' element={<Contact />} />
             <Route path='login' element={<LoginReg />} />
             <Route path='passwordresetrmail' element={<SendPasswordResetEmail />} />
+            <Route path='reset' element={<ResetPassword />} />
           </Route>
+          <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
       </Router>
     </>
