d78f6fa053f2738b9e525a70191009db60efdf55
AbhaySmaran <dasabhaysmaran@gmail.com>
2024-05-16 13:59:43 +0530

    updated commit

--- frontend/src/components/pages/LoginReg.jsx
+++ frontend/src/components/pages/LoginReg.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Grid,Card, Tab , Typography, Tabs } from '@mui/material';
+import { Box, Grid,Card, Tab ,  Tabs } from '@mui/material';
 import Pic1 from '../Image/pic1.png';
 import { useState } from 'react';
 import UserLogin from './auth/UserLogin';
@@ -37,7 +37,7 @@ const LoginReg = () => {
         </Grid>
         <Grid item lg={5} sm={7} xs={12}>
           <Card sx={{height: '100%' , width: '100%'}}>
-            <Box>
+            <Box sx={{mx: 3}}>
               <Box>
                 <Tabs value={value} textColor='secondary' indicatorColor='secondadry' onChange={handleChange}>
                   <Tab label='Login' sx={{textTransform:'none', fontWeight:'bold'}}></Tab>
