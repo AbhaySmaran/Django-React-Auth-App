90117097c86fc3bfc45085a7def52f2bcd4ccc7a
AbhaySmaran <dasabhaysmaran@gmail.com>
2024-07-10 23:56:32 +0530

    updated commit

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -7,6 +7,4 @@ from accounts.models import User
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     product_id = models.ForeignKey(Products, on_delete = models.CASCADE)
-    name = models.CharField(max_length=50, blank=True)
-    price = models.IntegerField()
-    image = models.ImageField()
+    
