c7814bce830220dd6e7d3ea1dd367ca172b33502
AbhaySmaran <dasabhaysmaran@gmail.com>
2024-07-14 22:40:58 +0530

    updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,8 +49,14 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    # 'DEFAULT_RENDERER_CLASSES':(
+    #     'rest_framework.renderers.JSONRenderers',
+    #     'rest_framework.renderers.BrowsableAPIRenderer'
+    # )
 }
+
+
     
 
 MIDDLEWARE = [
